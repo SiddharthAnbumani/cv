56e267987218ad1eb20302d0cb17a7ba27e3654b
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-03-27 17:54:28 +0530

    Update changes

--- src/components/GithubHeatMap.jsx
+++ src/components/GithubHeatMap.jsx
@@ -2,10 +2,8 @@ import GitHubCalendar from "react-github-calendar";
 
 export default function GitHubHeatmap() {
   return (
-    <div className="w-screen sm:h-auto  md:mx-20 flex justify-center mt-10 bg-black my-10 px-1 py-10 rounded-3xl">
-      <div className="scale-40 sm:scale-90 md:scale-90 lg:scale-110 xl:scale-125 2xl:scale-150">
-        <GitHubCalendar username="SiddharthAnbumani" />
-      </div>
+    <div className="w-screen sm:h-auto md:mx-20 flex justify-center mt-10 bg-black rounded-3xl  py-16 scale-40 sm:scale-90 md:scale-90 lg:scale-110 xl:scale-125 2xl:scale-150">
+      <GitHubCalendar username="SiddharthAnbumani" />
     </div>
   );
 }
