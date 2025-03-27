d5ca449389c9dd2acd83b01de540fa774a175efc
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-03-27 16:21:59 +0530

    Before deployment

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
-    include: ['framer-motion'], // Ensure framer-motion is pre-bundled
+    include: ['framer-motion','react-icons'], // Ensure framer-motion is pre-bundled
   },
 });
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),tailwindcss()],
-// })
